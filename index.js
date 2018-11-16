7f7a89e505e36b8e6ba714434a851c74af9a6844
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 04:53:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 /* Your code goes here */
-var customerName = 'bob'
+var customerName = 'bob';
 
 function upperCaseCustomerName() {
-  customerName = customerName.toUpperCase()
+  customerName = customerName.toUpperCase();
 }
 
-function setBestCustomer(){
-  var bestCustomer = 'not bob'
-} 
+function setBestCustomer() {
+ bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'soup'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not soup'
+}
